50e61c5ae0c65cbae892679ea2c1fa4d9018692f
1janisha <1janisha@gmail.com>
2024-02-26 21:27:30 +0000

    Added PhotoViewer Component - main image

---
+++ src/PhotoViewer/PhotoViewer.tsx
@@ -0,0 +1,9 @@
+import './PhotoViewer.css';
+
+export function PhotoViewer() {
+  return (
+    <div>
+      <img className="main-image" src="https://picsum.photos/id/237/600/400" alt="Image from Lorem Picsum" />
+    </div>
+  )
+}
