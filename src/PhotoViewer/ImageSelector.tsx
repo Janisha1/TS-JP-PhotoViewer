73097e0580e99569339f861bb8d04750e80ab561
1janisha <1janisha@gmail.com>
2024-02-28 23:38:50 +0000

    url of gallery image displayed when clicked

--- src/PhotoViewer/ImageSelector.tsx
+++ src/PhotoViewer/ImageSelector.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { imageUrls } from "./PhotoGallery";
 import './ImageSelector.css';
 
+// interface ImageSelectorProps {
+//   setImageUrl: (imageUrl: string) => void;
+// }
+
 function ImageSelector() {
+  const [imgSrc, setImgSrc] = useState('Choose an Image');
+
+  const handleClick = (imgSrc:string) => {
+    setImgSrc(imgSrc);
+  };
+
+
   if(imageUrls) {
     return (
       <div>
-        {imageUrls.map((imageUrl) => <img className="image-thumbnail" src={imageUrl} alt="Image Thumbnail"  />)}
+        <p>{'message is: ' + imgSrc}</p>
+        {/* {imageUrls.map((imageUrl) => <img className="image-thumbnail" onClick={handleClick} src={imageUrl} alt="Image Thumbnail"  />)} */}
+        {imageUrls.map((imageUrl) => <img className="image-thumbnail" onClick={() => handleClick(imageUrl)} src={imageUrl} alt="Image Thumbnail"  />)}
       </div>
     )
   }
